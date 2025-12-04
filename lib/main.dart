ec866d0e171e484e3f4b2b37809a77ef04a412a7
DoRenato <64191958+DoRenato@users.noreply.github.com>
2025-12-04 01:48:57 -0300

    Pagina inicial criada

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:atm_consultoria/TelaEmpresa.dart';
+import 'package:atm_consultoria/TelaContato.dart';
+import 'package:atm_consultoria/TelaServico.dart';
+import 'package:atm_consultoria/TelaCliente.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,6 +10,7 @@ void main() {
   ));
 }
 
+
 class PaginaPrincipal extends StatefulWidget {
   const PaginaPrincipal({super.key});
 
@@ -13,54 +18,95 @@ class PaginaPrincipal extends StatefulWidget {
   State<PaginaPrincipal> createState() => _PaginaPrincipalState();
 }
 
+
 class _PaginaPrincipalState extends State<PaginaPrincipal> {
+
+  void _abrirEmpresa(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TelaEmpresa()),
+    );
+  }
+
+  void _abrirServico(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TelaServico()),
+    );
+  }
+
+  void _abrirCliente(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TelaCliente()),
+    );
+  }
+
+  void _abrirContato(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TelaContato()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("ATM Consultoria"),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.green,
       ),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
             children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(top:30, bottom: 30),
+                      child: Image.asset("images/logo.png")
+                  ),
+                ],
+              ),
               Padding(
-                  padding: EdgeInsets.only(top:30, bottom: 30),
-                  child: Image.asset("images/logo.png")
+                padding: EdgeInsets.only(top: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap:_abrirEmpresa,
+                      child: Image.asset('images/menu_empresa.png'),
+                    ),
+                    GestureDetector(
+                        onTap:_abrirServico,
+                        child: Image.asset('images/menu_servico.png')
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap:_abrirCliente,
+                      child: Image.asset('images/menu_cliente.png'),
+                    ),
+                    GestureDetector(
+                        onTap:_abrirContato,
+                        child: Image.asset('images/menu_contato.png')
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                  padding: EdgeInsets.only(top: 40, bottom: 40, right: 40),
-                  child: Image.asset('images/menu_empresa.png'),
-              ),
-              Padding(
-                  padding: EdgeInsets.only(top: 40, bottom: 40),
-                  child: Image.asset('images/menu_servico.png')
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(bottom: 40, right: 40),
-                child: Image.asset('images/menu_cliente.png'),
-              ),
-              Padding(
-                  padding: EdgeInsets.only(bottom: 40),
-                  child: Image.asset('images/menu_contato.png')
-              ),
-            ],
-          ),
-        ],
-      ),
+        )
+      )
     );
   }
 }
